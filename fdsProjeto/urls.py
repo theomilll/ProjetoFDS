3742ca3767362713299ee6911f7407a56266213f
TheoMoura <99195030+theomilll@users.noreply.github.com>
2022-10-03 15:55:31 -0700

    Add files via upload
    Função de logout adicionada.

--- fdsProjeto/urls.py
+++ fdsProjeto/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("inventory/", include("fdsApp.urls")),
-    path("", auth_views.LoginView.as_view(template_name = "inventory_system/login.html"), name="login")
+    path("", auth_views.LoginView.as_view(template_name = "inventory_system/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name = "inventory_system/logout.html"), name="logout")
 ]
