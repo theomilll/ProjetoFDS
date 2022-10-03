7b82efbcfdd6b3cd2f94ae4027252d82420cdfe8
TheoMoura <99195030+theomilll@users.noreply.github.com>
2022-10-03 14:24:43 -0700

    Add files via upload
    redirect login

--- fdsProjeto/settings.py
+++ fdsProjeto/settings.py
@@ -118,8 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/inventory"
+LOGIN_URL = "login"
--- fdsProjeto/urls.py
+++ fdsProjeto/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("inventory/", include("fdsApp.urls")),
-    path("", auth_views.LoginView.as_view(template_name = "inventory_system/login.html"))
+    path("", auth_views.LoginView.as_view(template_name = "inventory_system/login.html"), name="login")
 ]
