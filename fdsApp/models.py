ed84f591f94de812853df76d01d7eecb60398d8a
TheoMoura <99195030+theomilll@users.noreply.github.com>
2022-10-03 08:04:53 -0700

    Add files via upload

---
+++ fdsApp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Inventory(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    quantity_in_stock = models.IntegerField(null=False, blank=False)
+    booked = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
+    book_date = models.DataField(auto_now_add=True)
+    
+    
+    def __str__(self) -> str:
+        return self.name
+    
