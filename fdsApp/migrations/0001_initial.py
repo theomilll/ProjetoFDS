e234854a0a6e344e4a3e0efbfecc7ea5b903eb47
TheoMoura <99195030+theomilll@users.noreply.github.com>
2022-10-03 11:54:04 -0700

    Add files via upload

--- fdsApp/migrations/0001_initial.py
+++ fdsApp/migrations/0001_initial.py
@@ -3,3 +3,20 @@
 from django.db import migrations, models
 
 
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='fdsApp',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('quantity_in_stock', models.IntegerField()),
+            ],
+        ),
+    ]
