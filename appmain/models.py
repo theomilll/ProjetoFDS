9aca33099a60c7c782419b5c52bac2640a5888aa
theomilll <tam4@cesar.school>
2022-10-03 07:38:37 -0700

    datastruct

--- appmain/models.py
+++ appmain/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Inventory(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    quantity_in_stock = models.IntegerField(null=False, blank=False)
+    booked = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
+    book_date = models.DataField(auto_now_add=True)
+    
+    
