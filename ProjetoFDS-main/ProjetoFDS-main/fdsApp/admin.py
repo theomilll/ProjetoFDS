26f67f9923060e2c057fdfee429219acdcf93b7d
TheoMoura <99195030+theomilll@users.noreply.github.com>
2022-10-18 15:56:45 -0700

    Delete ProjetoFDS-main/ProjetoFDS-main directory

--- ProjetoFDS-main/ProjetoFDS-main/fdsApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import fdsApp
-
-
-admin.site.register(fdsApp)
--- ProjetoFDS-main/ProjetoFDS-main/fdsApp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import inventory_list
-
-urlpatterns = [
-    path("", inventory_list, name="inventory_list")
-]
