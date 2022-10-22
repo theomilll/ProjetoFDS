f6f417d8f458623c0c02dd1179bdfe27e27d730d
TheoMoura <99195030+theomilll@users.noreply.github.com>
2022-10-22 12:07:11 -0700

    Add files via upload

--- fdsApp/views.py
+++ fdsApp/views.py
@@ -8,3 +8,6 @@ def inventory_list(request):
         "inventories": inventories
     }
     return render(request, "fds_app/inventory_list.html", context=context)
+
+def book1(request):
+    return render(request, 'book1.html')
