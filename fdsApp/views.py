ac46b74f4697e21ac54fa7317c77eea8d813fb59
theomilll <tam4@cesar.school>
2022-11-19 21:31:18 -0300

    controle de horario e datas de reserva de livros e pcs

--- fdsApp/views.py
+++ fdsApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import fdsApp, computers
+from datetime import datetime
+import pytz
 
 def inventory_list(request):
     inventories = fdsApp.objects.all()
@@ -11,16 +13,49 @@ def inventory_list(request):
     }
     return render(request, "fds_app/inventory_list.html", context=context)
 
-def book1(request):
-    return render(request, 'book1.html')
+def horarioDaReservaPC (request):
+    timezone = pytz.timezone('America/Sao_Paulo')
+    current_datetime = datetime.now(tz = timezone)
+    
+    dt = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    
+    str_current_datetime = str(dt)
+    
+    file_name = str_current_datetime
+    file = open("historico.txt", 'a')
+
+    file.write("PC reservado às : \n" + file_name + "\n\n")
+    file.close()
+    context = {
+        "title": "Horário da reserva",
+        "file_name": file_name,
+    }
+    
+    return render(request, 'pc1.html', context=context)
+
+def horarioDaReservaLivro (request):
+    timezone = pytz.timezone('America/Sao_Paulo')
+    current_datetime = datetime.now(tz = timezone)
+    
+    dt = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    
+    str_current_datetime = str(dt)
+    
+    file_name = str_current_datetime
+    file = open("historico.txt", 'a')
+
+    file.write("Livro reservado às : \n" + file_name + "\n\n")
+    file.close()
+    context = {
+        "title": "Horário da reserva",
+        "file_name": file_name,
+    }
+    
+    return render(request, 'book1.html', context=context)
 
 
 def book2(request):
     return render(request, 'book2.html')
-
-
-def pc1(request):
-    return render(request, 'pc1.html')
 
 
 def pc2(request):
